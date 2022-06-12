155121caf335fe94db783fb910a179bbd501ad70
Sinam <juansantos2kplus2@gmail.com>
2022-06-12 19:51:09 +0800

    basket summary delete function working

--- basket/basket.py
+++ basket/basket.py
@@ -1,5 +1,10 @@
 
 
+from decimal import Decimal
+from math import prod
+from store.models import Product
+
+
 class Basket():
     def __init__(self, request):
 
@@ -12,13 +17,51 @@ class Basket():
         self.basket = basket
 
     def add(self, product, qty):
+        # product_id = str(product.id)
+        # if product_id in self.basket:
+        #     self.basket[product_id]['qty'] = qty
+        # else:
+        #     self.basket[product_id] = {'price': str(product_id), 'qty': qty}
+
+        # self.save()
+
         product_id = product.id
         if product_id not in self.basket:
             self.basket[product_id] = {'price': str(
                 product.price), 'qty': int(qty)}
+        self.save()
 
-        self.session.modified = True
+    def __iter__(self):
+        product_ids = self.basket.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
 
     def __len__(self):
+        # sum = 0
 
+        # for item in self.basket.values():
+        #     sum += item['qty']
+        # #print(f'len: {sum}')
         return sum(item['qty'] for item in self.basket.values())
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+
+    def delete(self, product):
+        product_id = str(product)
+        print(f'product id: {product} \nself.basket: {self.basket}')
+        if product_id in self.basket:
+            del self.basket[product_id]
+            print("deleted successfully")
+            self.save()
+
+    def save(self):
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -5,8 +5,10 @@ from store.models import Product
 
 
 def basket_summary(request):
+    basket = Basket(request)
+    context = {'basket': basket}
 
-    return render(request, "store/basket/summary.html")
+    return render(request, "store/basket/summary.html", context)
 
 
 def basket_add(request):
@@ -21,4 +23,16 @@ def basket_add(request):
         basket_qty = basket.__len__()
         response = JsonResponse({'qty': basket_qty})
 
+        return response
+
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'POST':
+        product_id = int(request.POST.get('product_id'))
+        basket.delete(product=product_id)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
         return response
